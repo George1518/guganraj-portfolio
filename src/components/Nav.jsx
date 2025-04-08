7fe42dc6569c5666dcedd249d6dd56ccfc50dcee
George1518 <guganraj.me@gmail.com>
2025-04-08 11:46:59 +0530

    commited

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ function Nav() {
     <div className="container mx-auto flex justify-between items-center">
       <div className="text-2xl font-bold">Guganraj</div>
 
-      <div className="flex gap-10 sm:gap-4">
+      <div className="flex gap-10 sm:hidden">
         <a href="#home" className="hover:text-gray-400">
           Home
         </a>
