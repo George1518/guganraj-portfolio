afb5ae5cf9ca5bd0c4c0b296448bf59f61d527fd
George1518 <guganraj.me@gmail.com>
2025-04-08 11:40:11 +0530

    commited

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-
-
-
-export default defineConfig ( {
-  plugins: [ react(),
-    tailwindcss(),
-    autoprefixer(),
-  ], base: "/guganraj-portfolio"
-
-});
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
